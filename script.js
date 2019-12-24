25163f457d687d2453dbeecd7880919cecb8c682
Daniel Reynolds <50958520+camelCaseCo@users.noreply.github.com>
2019-12-23 20:23:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.alert('a');
-import { wallet } from 'nanocurrency-web';
+import { wallet } from '/node_modules/nanocurrency-web/dist/index.js';
 const abc = wallet.generate();
 window.alert('b');
 window.alert(abc.mnemonic);
